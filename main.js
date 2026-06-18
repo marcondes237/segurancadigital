bd5cc72f24b42280663aa9a7c3a769cf4e46741c
marcondes237 <fernanda.vendramin@escola.pr.gov.br>
2026-06-18 09:40:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -20,23 +20,20 @@ slider.addEventListener('input', () => {
 
 checkbox.forEach(cb => cb.addEventListener('change', gerarSenha));
 
-function gerarSenha(){
+function gerarSenha() {
 
     let alfabeto = "";
 
-    if(checkbox[0].checked) alfabeto += letrasMaiusculas;
-    if(checkbox[1].checked) alfabeto += letrasMinusculas;
-    if(checkbox[2].checked) alfabeto += numeros;
-    if(checkbox[3].checked) alfabeto += simbolos;
+    if (checkbox[0].checked) alfabeto += letrasMaiusculas;
+    if (checkbox[1].checked) alfabeto += letrasMinusculas;
+    if (checkbox[2].checked) alfabeto += numeros;
+    if (checkbox[3].checked) alfabeto += simbolos;
 
-    if(alfabeto.length === 0){
-        campoSenha.value = "";
-        return;
-    }
+    if (!alfabeto) return;
 
     let senha = "";
 
-    for(let i=0;i<tamanhoSenha;i++){
+    for (let i = 0; i < tamanhoSenha; i++) {
         const rand = Math.floor(Math.random() * alfabeto.length);
         senha += alfabeto[rand];
     }
@@ -46,24 +43,18 @@ function gerarSenha(){
     calcularForca(alfabeto.length);
 }
 
-function calcularForca(tamanhoAlfabeto){
+function calcularForca(tamanhoAlfabeto) {
 
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
 
     forcaSenha.classList.remove('fraca','media','forte');
 
-    if(entropia > 57){
-        forcaSenha.classList.add('forte');
-    } else if(entropia > 35){
-        forcaSenha.classList.add('media');
-    } else {
-        forcaSenha.classList.add('fraca');
-    }
-
-    const dias = Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24));
+    if (entropia > 57) forcaSenha.classList.add('forte');
+    else if (entropia > 35) forcaSenha.classList.add('media');
+    else forcaSenha.classList.add('fraca');
 
     entropiaEl.textContent =
-        `Pode levar até ${dias} dias para quebrar essa senha.`;
+        `Pode levar até ${Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24))} dias para quebrar.`;
 }
 
 gerarSenha();
