b5906013356e2dd2a34b6a0287cfc9332c40f335
marcondes237 <fernanda.vendramin@escola.pr.gov.br>
2026-06-22 11:55:07 -0300

    Refactor password generation logic and UI updates

--- main.js
+++ main.js
@@ -1,60 +1,77 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
-const slider = document.querySelector('#tamanho');
-const numeroSenha = document.querySelector('#numero-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
-const entropiaEl = document.querySelector('.entropia');
 
-let tamanhoSenha = 12;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
-const numeros = "0123456789";
-const simbolos = "!@#$%&*?";
-
-slider.addEventListener('input', () => {
-    tamanhoSenha = slider.value;
-    numeroSenha.textContent = tamanhoSenha;
-    gerarSenha();
-});
-
-checkbox.forEach(cb => cb.addEventListener('change', gerarSenha));
-
-function gerarSenha() {
-
-    let alfabeto = "";
-
-    if (checkbox[0].checked) alfabeto += letrasMaiusculas;
-    if (checkbox[1].checked) alfabeto += letrasMinusculas;
-    if (checkbox[2].checked) alfabeto += numeros;
-    if (checkbox[3].checked) alfabeto += simbolos;
-
-    if (!alfabeto) return;
-
-    let senha = "";
-
-    for (let i = 0; i < tamanhoSenha; i++) {
-        const rand = Math.floor(Math.random() * alfabeto.length);
-        senha += alfabeto[rand];
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
 
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
     campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
 
-    calcularForca(alfabeto.length);
 }
 
-function calcularForca(tamanhoAlfabeto) {
-
+function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-
+    console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
-
-    if (entropia > 57) forcaSenha.classList.add('forte');
-    else if (entropia > 35) forcaSenha.classList.add('media');
-    else forcaSenha.classList.add('fraca');
-
-    entropiaEl.textContent =
-        `Pode levar até ${Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24))} dias para quebrar.`;
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
-
-gerarSenha();
